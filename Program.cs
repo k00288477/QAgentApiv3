3364030b8372d9d36d09a86016b3f76a26b57c78
k00288477 <k00288477@student.tus.ie>
2026-01-09 20:30:34 +0000

    Add Production MySQL Support

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using QAgentApi.Data;
 using QAgentApi.Repository;
 using QAgentApi.Repository.Interfaces;
 using QAgentApi.Service;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,9 +14,31 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<AppDBContext>(options =>
-   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+// Database Provider
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Detect database provider
+if (connectionString.Contains("Server=localhost") || connectionString.Contains("SQLExpress"))
+{
+    // SQL Server (local development)
+    builder.Services.AddDbContext<AppDBContext>(options =>
+        options.UseSqlServer(connectionString)
+    );
+}
+else
+{
+    // MySQL (production on Railway)
+    builder.Services.AddDbContext<AppDBContext>(options =>
+        options.UseMySql(
+            connectionString,
+            ServerVersion.AutoDetect(connectionString)
+        )
+    );
+}
+
+//builder.Services.AddDbContext<AppDBContext>(options =>
+//   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+//);
 
 // Allow Requests from Local Vue app - Must Update for Production
 builder.Services.AddCors(options =>
