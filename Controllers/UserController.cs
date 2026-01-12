5c1beb88276234f670c8ab5f0e170fa018372404
k00288477 <k00288477@student.tus.ie>
2026-01-12 19:01:41 +0000

    Get User ProfileMethod added User Profile DTO added

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using QAgentApi.Model;
+using QAgentApi.Model.Dto;
 using QAgentApi.Service;
+using System.Security.Claims;
 
 namespace QAgentApi.Controllers
 {
@@ -16,11 +18,36 @@ namespace QAgentApi.Controllers
             _userService = userService;
         }
 
-        
-        [HttpGet("Test")]
-        public string TestMethod()
+
+        [HttpGet("getUserProfile")]
+        public async Task<ActionResult<UserProfileDto>> GetUserProfile()
         {
-            return "Api reached succesfully.";
+            //Get User Id from Claims
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId == null)
+            {
+                return Unauthorized("User not authorized");
+            }
+
+            // Get User by Id
+            var user = await _userService.GetUserById(int.Parse(userId));
+            // Check if user exists
+            if (user == null)
+            {
+                return NotFound("User Not Found");
+            }
+
+            // Map user to DTO (Exclude sensitive information)
+            var userProfile = new UserProfileDto
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+                OrganisationId = user.OrganisationId,
+                Organisation = user.Organisation
+            };
+            // return user
+            return Ok(userProfile);
         }
 
     }
---
+++ Model/Dto/UserProfileDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QAgentApi.Model.Dto
+{
+    public class UserProfileDto
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public int? OrganisationId { get; set; } // Foreign key to Organisation, nullable.
+        public Organisation? Organisation { get; set; } // User not required to belong to an organisation. 
+
+    }
+}
