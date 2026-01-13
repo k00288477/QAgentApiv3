39a20d163bb6221d266b95cf4547bc59baa3ee2d
k00288477 <148888532+k00288477@users.noreply.github.com>
2026-01-13 16:44:52 +0000

    Added Initial Migration

---
+++ Migrations/20260113164426_init.20260113.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QAgentApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class init20260113 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExecutionRuns",
+                columns: table => new
+                {
+                    ExecutionRunId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartTime = table.Column<TimeOnly>(type: "time", nullable: false),
+                    EndTime = table.Column<TimeOnly>(type: "time", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExecutionRuns", x => x.ExecutionRunId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Organisations",
+                columns: table => new
+                {
+                    OrgId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organisations", x => x.OrgId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestSuites",
+                columns: table => new
+                {
+                    TestSuiteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrganisationId = table.Column<int>(type: "int", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestSuites", x => x.TestSuiteId);
+                    table.ForeignKey(
+                        name: "FK_TestSuites_Organisations_OrganisationId",
+                        column: x => x.OrganisationId,
+                        principalTable: "Organisations",
+                        principalColumn: "OrgId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrganisationId = table.Column<int>(type: "int", nullable: true),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Organisations_OrganisationId",
+                        column: x => x.OrganisationId,
+                        principalTable: "Organisations",
+                        principalColumn: "OrgId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestCases",
+                columns: table => new
+                {
+                    TestCaseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TestSuiteId = table.Column<int>(type: "int", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: true),
+                    OrganisationOrgId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestCases", x => x.TestCaseId);
+                    table.ForeignKey(
+                        name: "FK_TestCases_Organisations_OrganisationOrgId",
+                        column: x => x.OrganisationOrgId,
+                        principalTable: "Organisations",
+                        principalColumn: "OrgId");
+                    table.ForeignKey(
+                        name: "FK_TestCases_TestSuites_TestSuiteId",
+                        column: x => x.TestSuiteId,
+                        principalTable: "TestSuites",
+                        principalColumn: "TestSuiteId");
+                    table.ForeignKey(
+                        name: "FK_TestCases_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExecutionReports",
+                columns: table => new
+                {
+                    ExecutionReportId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TestCaseId = table.Column<int>(type: "int", nullable: false),
+                    ExecutionDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExecutedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    ExecutionRunId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExecutionReports", x => x.ExecutionReportId);
+                    table.ForeignKey(
+                        name: "FK_ExecutionReports_ExecutionRuns_ExecutionRunId",
+                        column: x => x.ExecutionRunId,
+                        principalTable: "ExecutionRuns",
+                        principalColumn: "ExecutionRunId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ExecutionReports_TestCases_TestCaseId",
+                        column: x => x.TestCaseId,
+                        principalTable: "TestCases",
+                        principalColumn: "TestCaseId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestSteps",
+                columns: table => new
+                {
+                    StepId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Index = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TestCaseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestSteps", x => x.StepId);
+                    table.ForeignKey(
+                        name: "FK_TestSteps_TestCases_TestCaseId",
+                        column: x => x.TestCaseId,
+                        principalTable: "TestCases",
+                        principalColumn: "TestCaseId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    CommentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateTimeCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExecutionReportId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.CommentId);
+                    table.ForeignKey(
+                        name: "FK_Comments_ExecutionReports_ExecutionReportId",
+                        column: x => x.ExecutionReportId,
+                        principalTable: "ExecutionReports",
+                        principalColumn: "ExecutionReportId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ExecutionReportId",
+                table: "Comments",
+                column: "ExecutionReportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExecutionReports_ExecutionRunId",
+                table: "ExecutionReports",
+                column: "ExecutionRunId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExecutionReports_TestCaseId",
+                table: "ExecutionReports",
+                column: "TestCaseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestCases_OrganisationOrgId",
+                table: "TestCases",
+                column: "OrganisationOrgId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestCases_TestSuiteId",
+                table: "TestCases",
+                column: "TestSuiteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestCases_UserId",
+                table: "TestCases",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestSteps_TestCaseId",
+                table: "TestSteps",
+                column: "TestCaseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestSuites_OrganisationId",
+                table: "TestSuites",
+                column: "OrganisationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_OrganisationId",
+                table: "Users",
+                column: "OrganisationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "TestSteps");
+
+            migrationBuilder.DropTable(
+                name: "ExecutionReports");
+
+            migrationBuilder.DropTable(
+                name: "ExecutionRuns");
+
+            migrationBuilder.DropTable(
+                name: "TestCases");
+
+            migrationBuilder.DropTable(
+                name: "TestSuites");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Organisations");
+        }
+    }
+}
