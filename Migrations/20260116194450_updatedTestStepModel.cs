b323d710506e2f8a9644b92726dcedecb04a8968
k00288477 <k00288477@student.tus.ie>
2026-01-16 19:46:50 +0000

    Updated TestStep Model and added DB Migration

---
+++ Migrations/20260116194450_updatedTestStepModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QAgentApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedTestStepModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "TestSteps",
+                newName: "ExpectedResult");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Action",
+                table: "TestSteps",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Action",
+                table: "TestSteps");
+
+            migrationBuilder.RenameColumn(
+                name: "ExpectedResult",
+                table: "TestSteps",
+                newName: "Content");
+        }
+    }
+}
